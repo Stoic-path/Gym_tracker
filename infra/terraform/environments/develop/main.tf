d11dab01b3effd345cb9fb280761f5cd5be433bc
Stoic-Path <jasaltosv@uce.edu.ec>
2026-01-21 19:41:50 -0500

    fix(infra): correct syntax error in aws_ecs_cluster block

--- infra/terraform/environments/develop/main.tf
+++ infra/terraform/environments/develop/main.tf
@@ -507,7 +507,10 @@ resource "aws_ecr_repository" "services" {
 
 resource "aws_ecs_cluster" "main" {
   name = "${var.project_name}-cluster"
-  setting { name  = "containerInsights"; value = "enabled" }
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
 }
 
 resource "aws_cloudwatch_log_group" "ecs_logs" {
